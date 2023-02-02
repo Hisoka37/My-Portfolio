ed3c29794ba888dfcfb8f28c42da63ba22f0d6ba
Walid Kabboussa <walidkabou@gmail.com>
2023-02-02 12:54:12 +0100

    Fix linters

--- form.js
+++ form.js
@@ -11,7 +11,7 @@ function validate() {
       document.getElementById('error').style.visibility = 'visible';
       document.getElementById('error').style.color = '#f63778';
       document.getElementById('error').style.fontSize = '20px';
-      e.preventDefault();      
+      e.preventDefault();
     }
   };
 }
